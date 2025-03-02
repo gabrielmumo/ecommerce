79d05b729f017bf23cc7121aa48bb71f1b67ae65
gabrielmumo <gabrielmumo@gabrielmumo.dev>
2025-03-01 22:50:30 -0500

    Toggles theme mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from 'next-themes';
 import '@/assets/styles/globals.css';
 import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from '@/lib/constants';
 
@@ -21,7 +22,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='light'
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      </body>
     </html>
   );
 }
